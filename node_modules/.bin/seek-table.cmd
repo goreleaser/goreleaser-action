1f2ea644cc0465941672be729c669e5a1868cc9a
CrazyMax <crazy-max@users.noreply.github.com>
2020-04-09 03:25:32 +0200

    Prune node_modules

--- node_modules/.bin/seek-table.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\seek-bzip\bin\seek-bzip-table" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\seek-bzip\bin\seek-bzip-table" %*
-)
