81416cf65a480b38c7d6d414a15d27c924cf339c
Radek Simko <radek.simko@gmail.com>
2020-06-06 18:12:52 +0100

    fix: make tests pass outside of GitHub Action env

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
-  setupFiles: ["dotenv/config"],
+  setupFiles: [
+    "dotenv/config",
+    "<rootDir>/src/test_setup.ts"
+  ],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
