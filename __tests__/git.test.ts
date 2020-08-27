564b0a07c93adabe60d78e3ed42f0e53f1319830
CrazyMax <crazy-max@users.noreply.github.com>
2020-08-27 04:23:37 +0200

    Update tests

--- __tests__/git.test.ts
+++ __tests__/git.test.ts
@@ -1,10 +1,16 @@
 import * as git from '../src/git';
 
 describe('git', () => {
-  it('returns git tag', async () => {
+  it('returns git tag through describe', async () => {
     const tag: string = await git.getTag();
     console.log(`tag: ${tag}`);
     expect(tag).not.toEqual('');
+  });
+  it('returns git tag through GITHUB_REF', async () => {
+    process.env.GITHUB_REF = 'refs/tags/v2.2.1'
+    const tag: string = await git.getTag();
+    console.log(`tag: ${tag}`);
+    expect(tag).toEqual('v2.2.1');
   });
   it('checks if tag is dirty', async () => {
     expect(await git.isTagDirty('v1.3.1')).toBe(true);
