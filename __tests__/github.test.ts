c5666875d0f143c032a31908072792f77c701b81
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-05-08 16:22:50 -0300

    feat: default to "~> v1" instead of "latest"
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals';
+import {describe, expect, it} from '@jest/globals';
 import * as github from '../src/github';
 
 describe('getRelease', () => {
